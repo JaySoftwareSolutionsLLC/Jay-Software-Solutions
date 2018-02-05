86b7cb7d3a0cb39fdc8ef0a4095ffaf28dff5ebe
JaySoftwareSolutionsLLC <Brettbrewster@jaysoftwaresolutions.com>
2018-02-05 02:19:55 -0500

    adding a setTimeout to the orbs

--- resources/javascript/moving_orbs.js
+++ resources/javascript/moving_orbs.js
@@ -6,44 +6,46 @@ $(document).ready(function() {
 		let maxVy = 20;
 		let numOfOrbs = 4;
 		let minOpacity = 0.4;
-		let maxOpacity = 1;
+		let maxOpacity = 0.6;
 		
 //	GOAL: Create a few (3-5) orbs which will explode from the center of the Elegantly Effective h2 element and then move around the jumbotron bouncing off of the walls when they come in contact with them
 
 	 function floaterFunctionality() {
 		let floaterFPS = fps;
-		var floaterDisplaySize = parseInt($('html').css("font-size"));	//Set size to 1rem for boundries, size of elements is set in css
-		var jumbotronElement = $("div.jumbotron");
-		var jumbotronWidth = jumbotronElement.width();
-		var jumbotronHeight = jumbotronElement.height();
-		var jumbotronStart = $("header").height();
-		var jumbotronEnd = jumbotronStart + jumbotronHeight;
-		var jumbotronTitleElement = $("div.jumbotron h2");
-		var jumbotronTitleStartX = $(jumbotronTitleElement).offset().left;
-		var jumbotronTitleStartY = $(jumbotronTitleElement).offset().top  - $(document).scrollTop();
-		var jumbotronTitleWidth = $(jumbotronTitleElement).width();
-		var jumbotronTitleHeight = $(jumbotronTitleElement).height();
+		let floaterDisplaySize = parseInt($('html').css("font-size"));	//Set size to 1rem for boundries, size of elements is set in css
+		let jumbotronElement = $("div.jumbotron");
+		let jumbotronWidth = jumbotronElement.width();
+		let jumbotronHeight = jumbotronElement.height();
+		let jumbotronStart = $("header").height();
+		let jumbotronEnd = jumbotronStart + jumbotronHeight;
+		let jumbotronTitleElement = $("div.jumbotron h2");
+		let jumbotronTitleStartX = $(jumbotronTitleElement).offset().left;
+		let jumbotronTitleStartY = $(jumbotronTitleElement).offset().top  - $(document).scrollTop();
+		let jumbotronTitleWidth = $(jumbotronTitleElement).width();
+		let jumbotronTitleHeight = $(jumbotronTitleElement).height();
 		function CreateFloater() {
 			this.x = Math.round((Math.random() * jumbotronWidth));
 			this.y = jumbotronStart + Math.round((Math.random() * jumbotronHeight));
 			this.Vx = (-1 * maxVx) + Math.round(Math.random() * (maxVx * 2));
 			this.Vy = (-1 * maxVy) + Math.round(Math.random() * (maxVy * 2));
+			this.Ax = 0;
+			this.Ay = 0;
 		}
-		var floaterArray = [];
+		let floaterArray = [];
 		function generateNewFloater() {
-			var floater = document.createElement("div");
+			let floater = document.createElement("div");
 			floaterArray.push(new CreateFloater());
 			floater.className += "floater";
-			var x = jumbotronTitleStartX + (jumbotronTitleWidth - 50) + 'px';
-			var y = jumbotronTitleStartY + (jumbotronTitleHeight / 2) + 'px';
+			let x = jumbotronTitleStartX + (jumbotronTitleWidth - 50) + 'px';
+			let y = jumbotronTitleStartY + (jumbotronTitleHeight / 2) + 'px';
 			floater.style.left = x;
 			floater.style.top = y;
-			var opacity = minOpacity + (Math.floor(Math.random() * ((maxOpacity - minOpacity) * 10)) / 10);
+			let opacity = minOpacity + (Math.floor(Math.random() * ((maxOpacity - minOpacity) * 10)) / 10);
 			floater.style.opacity = opacity;
 			$('body').append(floater);
 		}
 		function generateFloaters(number) {
-			for (var i = 0; i < number; i++) {
+			for (let i = 0; i < number; i++) {
 				generateNewFloater();
 			}
 		}
@@ -70,11 +72,11 @@ $(document).ready(function() {
 			return true;
 		}
 		function createFloaterMovement() {
-			var floaters = $(".floater");
+			let floaters = $(".floater");
 			setInterval(function(){
-				for (var i = 0; i < floaters.length; i++) {
-					var x = parseInt($(floaters[i]).css("left"), 10);
-					var y = parseInt($(floaters[i]).css("top"), 10);
+				for (let i = 0; i < floaters.length; i++) {
+					let x = parseInt($(floaters[i]).css("left"), 10);
+					let y = parseInt($(floaters[i]).css("top"), 10);
 					x += floaterArray[i].Vx;
 					y += floaterArray[i].Vy;
 					if (checkXParameter(x)) {}
@@ -90,9 +92,15 @@ $(document).ready(function() {
 				}
 			}, 1000/floaterFPS);
 		}
+//		 function returnMouseCoordinates() {
+//			 let mouseX = document.pageX;
+//			 console.log(mouseX);
+//			 return event.pageX;
+//		 }
+//		 returnMouseCoordinates();
 		generateFloaters(numOfOrbs);
 		createFloaterMovement();
 	}
-	floaterFunctionality();
+	setTimeout(floaterFunctionality, 500);
 	})();
 });
